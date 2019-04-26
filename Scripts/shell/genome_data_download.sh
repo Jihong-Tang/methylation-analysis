4897868366a4f69f2031c46d3ff47693e2563a77
Jihong <njutangjihong@gmail.com>
2019-04-26 16:09:59 +0800

    Update genome_data_download.sh

--- Scripts/shell/genome_data_download.sh
+++ Scripts/shell/genome_data_download.sh
@@ -4,12 +4,13 @@
 # Author: Jihong Tang
 # Date: April 23, 2019
  
-prefix = 'ftp://ftp.ensembl.org/pub/release-96/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.chromosome.'
-suffix = '.fa.gz'
+prefix='ftp://ftp.ensembl.org/pub/release-96/fasta/mus_musculus/dna/Mus_musculus.GRCm38.dna.chromosome.'
+suffix='.fa.gz'
 
 for chr in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 X Y
 do 
-    ftpadd = "${prefix}${chr}${suffix}"
-    aria2c -d $1 -Z ftpadd
+    ftpadd="${prefix}${chr}${suffix}"
+   # echo $ftpadd
+    aria2c -d $1 -Z $ftpadd
 done
 
